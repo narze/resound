b4d84c66643e68d5c3dae868577b86cba2dd9d46
Ronnapatp <84321166+ronnapatp@users.noreply.github.com>
2021-08-03 23:19:34 +0700

    Add files via upload

--- src/App.svelte
+++ src/App.svelte
@@ -23,8 +23,9 @@
 
 <main>
   <h1>Remote Soundboard</h1>
+  
 
-  <ul>
+  <ul class="sound">
     {#each audioArray as audio }
       <li>{audio.name} <Sound label={audio.name} filename={audio.name} /></li>
     {/each}
@@ -32,6 +33,30 @@
 
   <button on:click={addNewAudio}>Add New Audio</button>
   <button on:click={clearAudio}>Remove all audios</button>
+
+  <footer>
+    <a href="https://github.com/narze/remote-soundboard" target="_blank" rel="noreferrer">Github</a>
+    |
+    <a href="https://twitter.com/narze" target="_blank" rel="noneferrer">@narze</a>
+    |
+    <a href="https://watasalim.vercel.app" target="_blank" rel="noreferrer">วาทะสลิ่มสุดเจ๋ง</a>
+    |
+    <a href="https://tid-vs-truad.vercel.app" target="_blank" rel="noreferrer">ติด vs ตรวจ</a>
+    |
+    <a href="https://9speech.vercel.app" target="_blank" rel="noreferrer">9speech</a>
+    |
+    <a href="https://thailand-grand-opening.web.app" target="_blank" rel="noreferrer">ลุงจะใช้เวลาเท่าไหร่ในการเปิดประเทศ?</a>
+  </footer>
+
+  <script src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js"></script>
+  <script>
+    kofiWidgetOverlay.draw("narze", {
+      type: "floating-chat",
+      "floating-chat.donateButton.text": "สนับสนุน ฿",
+      "floating-chat.donateButton.background-color": "#f45d22",
+      "floating-chat.donateButton.text-color": "#fff",
+    });
+  </script>
 </main>
 
 <style>
@@ -66,7 +91,18 @@
     margin: 1rem auto;
     line-height: 1.35;
   }
-
+  button{
+    background-color: #ff3e00; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  }
+  footer{color:black;position:fixed;right:10px;bottom:10px;font-size:1rem}
+  a{color: black;text-decoration: none;}
   @media (min-width: 480px) {
     h1 {
       max-width: none;
@@ -76,4 +112,6 @@
       max-width: none;
     }
   }
+
+  
 </style>
